b303d6b384cceb1b2af3da0f7ebd83a6401a56f3
laksh47 <lakshmanansathya@gmail.com>
2025-12-02 01:32:57 -0500

    solution

--- aoc2k25/day2/day2.py
+++ aoc2k25/day2/day2.py
@@ -3,29 +3,56 @@ from pathlib import Path
 
 # https://adventofcode.com/2025/day/2
 
+# def is_invalid_id(id_val):
+#     s = str(id_val)
+#     length = len(s)
+
+#     if length % 2 != 0:
+#         return False
+
+#     midpoint = length // 2
+#     first_half = s[:midpoint]
+#     second_half = s[midpoint:]
+
+#     return first_half == second_half
+
+
+def is_invalid_id(id_val):
+    s = str(id_val)
+    length = len(s)
+
+    for i in range(1, length // 2 + 1):
+        if length % i == 0:
+            pattern = s[:i]
+            repeats = length // i
+
+            if pattern * repeats == s:
+                return True
+
+    return False
+
+
+def find_invalid_ids_sum(input_test: str):
+    invalid_ids = []
+    for range_str in input_test.split(","):
+        start, end = range_str.split("-")
+        for id in range(int(start), int(end) + 1):
+            if is_invalid_id(id):
+                invalid_ids.append(id)
+    return sum(invalid_ids)
+
 
 def main(filename: str = "input_test.txt"):
-    print(f"Processing file: {filename}")
     base_dir = Path(__file__).resolve().parent
     file_path = base_dir / filename
-
     with open(file_path, "r") as file:
-        lines = file.readlines()
+        input_test = file.read()
 
-    for line in lines:
-        line = line.strip()
-        if not line:
-            continue
-        # Process each line here
-        print(f"Processing: {line}")
-
-    print("Result: ")
+    print(f"The sum of the invalid IDs is {find_invalid_ids_sum(input_test)}")
 
 
 if __name__ == "__main__":
-    # If a filename is provided as the first CLI argument, use it; otherwise use the default.
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
         main()
-
