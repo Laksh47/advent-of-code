371d7aa6100acfc5538754a0f999e309708e5128
laksh47 <lakshmanansathya@gmail.com>
2025-12-03 00:23:48 -0500

    day 3 part 1

--- aoc2k25/day3/day3.py
+++ aoc2k25/day3/day3.py
@@ -4,6 +4,16 @@ from pathlib import Path
 # https://adventofcode.com/2025/day/3
 
 
+def get_largest_joltage(bank: str):
+    max_joltage = 0
+    for i in range(len(bank)):
+        for j in range(i + 1, len(bank)):
+            joltage = int(bank[i] + bank[j])
+            if joltage > max_joltage:
+                max_joltage = joltage
+    return max_joltage
+
+
 def main(filename: str = "input_test.txt"):
     print(f"Processing file: {filename}")
     base_dir = Path(__file__).resolve().parent
@@ -12,14 +22,19 @@ def main(filename: str = "input_test.txt"):
     with open(file_path, "r") as file:
         lines = file.readlines()
 
+    sum_joltage = 0
     for line in lines:
         line = line.strip()
         if not line:
             continue
         # Process each line here
         print(f"Processing: {line}")
+        curr_max_joltage = get_largest_joltage(line)
+        print(f"The largest joltage for bank {line} is {curr_max_joltage}")
+        print()
+        sum_joltage += curr_max_joltage
 
-    print("Result: ")
+    print(f"The total output joltage is {sum_joltage}")
 
 
 if __name__ == "__main__":
