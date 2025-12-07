8c8ec021a5bda64530f5bf30dc06727d8c06c776
laksh47 <lakshmanansathya@gmail.com>
2025-12-07 07:35:34 -0500

    day7, part 2

--- aoc2k25/day7/day7.py
+++ aoc2k25/day7/day7.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 from pathlib import Path
 
 # https://adventofcode.com/2025/day/7
@@ -25,7 +26,6 @@ def solve_tachyon_manifold(grid_input: list[str]) -> int:
         if grid_input[0][c] == "S":
             start_col = c
             break
-
 
     active_beams = {start_col}
     split_count = 0
@@ -55,6 +55,65 @@ def solve_tachyon_manifold(grid_input: list[str]) -> int:
     return split_count
 
 
+def quantum_split(grid_input, start_col):
+    R = len(grid_input)
+    C = len(grid_input[0])
+
+    timeline_counts = defaultdict(int)
+    timeline_counts[start_col] = 1
+
+    total_completed_timelines = 0
+
+    for r in range(1, R):
+        next_timeline_counts = defaultdict(int)
+
+        for c, count in timeline_counts.items():
+            if c < 0 or c >= C:
+                total_completed_timelines += count
+                continue
+
+            cell = grid_input[r][c]
+
+            if cell == "S" or cell == ".":
+                next_timeline_counts[c] += count
+
+            elif cell == "^":
+                left_target = c - 1
+                right_target = c + 1
+
+                if 0 <= left_target < C:
+                    next_timeline_counts[left_target] += count
+                else:
+                    total_completed_timelines += count
+
+                if 0 <= right_target < C:
+                    next_timeline_counts[right_target] += count
+                else:
+                    total_completed_timelines += count
+
+        timeline_counts = next_timeline_counts
+
+        if not timeline_counts:
+            break
+
+    total_completed_timelines += sum(timeline_counts.values())
+    return total_completed_timelines
+
+
+def solve_quantum_tachyon_manifold(grid_input):
+    if not grid_input:
+        return 0
+
+    C = len(grid_input[0])
+    start_col = -1
+    for c in range(C):
+        if grid_input[0][c] == "S":
+            start_col = c
+            break
+
+    return quantum_split(grid_input, start_col)
+
+
 def main(filename: str = "input_test.txt"):
     print(f"Processing file: {filename}")
     base_dir = Path(__file__).resolve().parent
@@ -65,6 +124,9 @@ def main(filename: str = "input_test.txt"):
 
     total_splits = solve_tachyon_manifold(lines)
     print(f"Total splits: {total_splits}")
+
+    total_completed_timelines = solve_quantum_tachyon_manifold(lines)
+    print(f"Total timelines: {total_completed_timelines}")
 
 
 if __name__ == "__main__":
